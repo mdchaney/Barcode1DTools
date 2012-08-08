5af1afca53e362e8f652f30c9ce1fb865fd80a53
Michael Chaney <mdchaney@michaelchaney.com>
2012-08-08 11:53:40 -0500

    Added documentation.

--- lib/barcode1dtools.rb
+++ lib/barcode1dtools.rb
@@ -20,6 +20,31 @@ module Barcode1DTools
   #  "x x   xx x  xx  x  x  xx x xxxx xxx xx x xxxx x x x  xxx xx  xx xxx  x xx xx  xx xx  x x    x x"
   #  ean13.rle
   #  "11132112221212211141312111411111123122213211212221221114111"
+  #
+  #== Standard Options
+  #  When creating a barcode, there are a number of options available:
+  #
+  #  1. checksum_included - The checksum is included in the value
+  #     and does not need to be generated.  This checksum will be
+  #     validated and an error raised if it is not proper.
+  #  2. line_character, space_character - when generating a bar
+  #     pattern, determines the characters which will represent bars
+  #     and spaces in the pattern.  These default to "1" for lines and
+  #     "0" for spaces.
+  #  3. w_character, n_character - When generating a w/n pattern,
+  #     determines the characters to be used for wide and narrow
+  #     bars and spaces.  Defaults to "w" and "n".  Not applicable to
+  #     all barcode types.
+  #
+  #== Standard Object Accessors
+  #  1. Barcode1D#value - The actual value of the payload.  If there
+  #     is a checksum, it is not part of the value.  This may be a
+  #     string or an integer depending on the type of barcode.
+  #  2. Barcode1D#check_digit - The checksum digit (or digits).
+  #     This is an integer.
+  #  3. Barcode1D#encoded_string - The entire literal value that is
+  #     encoded, including check digit(s).
+
 
   # Errors for barcodes
   class Barcode1DError < StandardError; end
