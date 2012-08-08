3333dfda98887a45a80e33579eabae7f11f3c902
Michael Chaney <mdchaney@michaelchaney.com>
2012-08-08 12:27:55 -0500

    Completed EAN-13 test suite.

--- test/test_barcode1dean13.rb
+++ test/test_barcode1dean13.rb
@@ -38,9 +38,28 @@ class Barcode1DToolsEAN13Test < Test::Unit::TestCase
     assert_raise(Barcode1DTools::ChecksumError) { Barcode1DTools::EAN13.new('0884088516331', :checksum_included => true) }
   end
 
+  def test_value_length_errors
+    # One digit too short
+    assert_raise(Barcode1DTools::UnencodableCharactersError) { Barcode1DTools::EAN13.new('01234567890', :checksum_included => false) }
+    assert_raise(Barcode1DTools::UnencodableCharactersError) { Barcode1DTools::EAN13.new('012345678901', :checksum_included => true) }
+    # One digit too long
+    assert_raise(Barcode1DTools::UnencodableCharactersError) { Barcode1DTools::EAN13.new('0123456789012', :checksum_included => false) }
+    assert_raise(Barcode1DTools::UnencodableCharactersError) { Barcode1DTools::EAN13.new('01234567890123', :checksum_included => true) }
+  end
+
+  def test_width
+    ean = Barcode1DTools::EAN13.new('0041343005796', :checksum_included => true)
+    assert_equal 95, ean.width
+  end
+
   def test_barcode_generation
     ean = Barcode1DTools::EAN13.new('0012676510226', :checksum_included => true)
     assert_equal "10100011010011001001001101011110111011010111101010100111011001101110010110110011011001010000101", ean.bars
     assert_equal "11132112221212211141312111411111123122213211212221221114111", ean.rle
+  end
+
+  def test_wn_raises_error
+    ean = Barcode1DTools::EAN13.new('0012676510226', :checksum_included => true)
+    assert_raise(Barcode1DTools::NotImplementedError) { ean.wn }
   end
 end
